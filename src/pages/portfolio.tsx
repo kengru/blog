06a8ac6845a9ab42b084d326bc721b4060ab56d3
Kendry Grullon <kengrullon@gmail.com>
2021-03-26 11:47:23 -0400

    Changed the ico of the page.

---
+++ src/pages/portfolio.tsx
@@ -0,0 +1,98 @@
+import React from "react";
+import { PageProps, Link, graphql } from "gatsby";
+
+import Bio from "../components/bio";
+import Layout from "../components/layout";
+import SEO from "../components/seo";
+
+const Portfolio: React.FC<PageProps<IndexData>> = ({ data, location }) => {
+  const siteTitle = data.site.siteMetadata.title || `Title`;
+  const posts = data.allMarkdownRemark.nodes;
+
+  if (posts.length === 0) {
+    return (
+      <Layout location={location} title={siteTitle}>
+        <SEO title="All posts" description="All posts" />
+        <Bio />
+        <p>
+          No blog posts found. Add markdown posts to "content/blog" (or the
+          directory you specified for the "gatsby-source-filesystem" plugin in
+          gatsby-config.js).
+        </p>
+      </Layout>
+    );
+  }
+
+  return (
+    <Layout location={location} title={siteTitle}>
+      <SEO title="All posts" description="All posts" />
+      <Bio />
+      <ol style={{ listStyle: `none` }}>
+        {posts.map(post => {
+          const title = post.frontmatter.title || post.fields.slug;
+
+          return (
+            <li key={post.fields.slug}>
+              <article
+                className="post-list-item"
+                itemScope
+                itemType="http://schema.org/Article"
+              >
+                <header>
+                  <h2>
+                    <Link to={post.fields.slug} itemProp="url">
+                      <span itemProp="headline">{title}</span>
+                    </Link>
+                  </h2>
+                  <small>
+                    {post.frontmatter.date}
+                    {post.frontmatter.tags &&
+                      post.frontmatter.tags.split(" ").map(tag => (
+                        <span key={tag} className="tag">
+                          {tag}
+                        </span>
+                      ))}
+                  </small>
+                </header>
+                <section>
+                  <p
+                    dangerouslySetInnerHTML={{
+                      __html: post.frontmatter.description || post.excerpt
+                    }}
+                    itemProp="description"
+                  />
+                </section>
+              </article>
+            </li>
+          );
+        })}
+      </ol>
+    </Layout>
+  );
+};
+
+export default Portfolio;
+
+export const pageQuery = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
+      nodes {
+        excerpt
+        fields {
+          slug
+        }
+        frontmatter {
+          date(formatString: "MMMM DD, YYYY")
+          title
+          description
+          tags
+        }
+      }
+    }
+  }
+`;
